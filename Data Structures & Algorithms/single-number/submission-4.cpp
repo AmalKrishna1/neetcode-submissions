dec454eab461184051239f6c988a80bd92a08275
Amal Krishna <66444212+AmalKrishna1@users.noreply.github.com>
2026-07-09 18:04:54 -0700

    Add: single-number - submission-4

---
+++ Data Structures & Algorithms/single-number/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+
+        std::sort(nums.begin(), nums.end());
+        int single_number = 0; //set a variable for tracking
+ 
+        for (int num : nums){ //navigate through the for loop
+            //Run XOR on EVERY value in the for loop to evaluate for the single number.
+            //This will evaluate out to that valid single number because of the XOR properties.
+            //Since there will always be an even number excluding the one single, that specific section
+            //will always evaluate to zero, thus the only true remaining value will be a that individual
+            //single number.
+            //e.g.
+
+            /*
+                4 - 100
+                1 - 001
+                2 - 010
+                1 - 001
+                2 - 010
+
+                Alright, so in the above, looking specifically at column three, the - 1, 0, 1, 0 sequece will
+                all evaluate to 0 leaving just the 0 at the top for the single number four. That 0 XOR'd with 
+                the accumulated 0 will output a 0. This is the logic for all three columns eventually reaching
+                a total sum of 100 which is 4, the correct output.
+            */
+            single_number ^= num;
+        }
+            return single_number;
+    }
+};
